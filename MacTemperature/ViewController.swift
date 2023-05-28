0f1cf7d51aafcedd26b89462f3aee18fb6c47f07
ighiba <ighiba18@gmail.com>
2023-05-28 21:15:18 +0300

    Initial Commit

---
+++ MacTemperature/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacTemperature
+//
+//  Created by Ivan Ghiba on 28.05.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
