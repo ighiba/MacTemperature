2b99d3f2692f1101803bf38ea278fbba11b84129
ighiba <ighiba18@gmail.com>
2023-05-30 02:32:10 +0300

    Updated AppleSMC

--- MacTemperature/Model/AppleSMC.swift
+++ MacTemperature/Model/AppleSMC.swift
@@ -37,4 +37,133 @@ class AppleSMC {
             fatalError("Error")
         }
     }
+    
+    func read(_ value: UnsafeMutablePointer<SMCVal_t>) -> kern_return_t {
+        
+        let kernelIndex: UInt32 = 2
+        let readBytes: UInt32 = 5
+        let readKeyInfo: UInt8 = 9
+                
+        var input = SMCKeyData_t()
+        var output = SMCKeyData_t()
+        
+        input.key = FourCharCode(fromString: value.pointee.key)
+        input.data8 = readKeyInfo
+
+        var result: kern_return_t
+        
+        result = AppleSMC.shared.call(kernelIndex, input: &input, output: &output)
+        if result != kIOReturnSuccess {
+            fatalError("Error")
+        }
+        
+        value.pointee.dataSize = UInt32(output.keyInfo.dataSize)
+        value.pointee.dataType = output.keyInfo.dataType.toString()
+        input.keyInfo.dataSize = output.keyInfo.dataSize
+        input.data8 = UInt8(readBytes)
+        
+        result = AppleSMC.shared.call(kernelIndex, input: &input, output: &output)
+        if result != kIOReturnSuccess {
+            fatalError("Error")
+        }
+
+        memcpy(&value.pointee.bytes, &output.bytes, Int(value.pointee.dataSize))
+        
+        return result
+    }
+    
+    func call(_ index: UInt32, input: inout SMCKeyData_t, output: inout SMCKeyData_t) -> kern_return_t {
+        let inputSize = MemoryLayout<SMCKeyData_t>.stride
+        var outputSize = MemoryLayout<SMCKeyData_t>.stride
+
+        return IOConnectCallStructMethod(connection, index, &input, inputSize, &output, &outputSize)
+    }
 }
+
+internal struct SMCKeyData_t {
+    typealias SMCBytes_t = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8,
+                            UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8,
+                            UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8,
+                            UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8,
+                            UInt8, UInt8, UInt8, UInt8)
+    
+    struct vers_t {
+        var major: CUnsignedChar = 0
+        var minor: CUnsignedChar = 0
+        var build: CUnsignedChar = 0
+        var reserved: CUnsignedChar = 0
+        var release: CUnsignedShort = 0
+    }
+    
+    struct LimitData_t {
+        var version: UInt16 = 0
+        var length: UInt16 = 0
+        var cpuPLimit: UInt32 = 0
+        var gpuPLimit: UInt32 = 0
+        var memPLimit: UInt32 = 0
+    }
+    
+    struct keyInfo_t {
+        var dataSize: IOByteCount32 = 0
+        var dataType: UInt32 = 0
+        var dataAttributes: UInt8 = 0
+    }
+    
+    var key: UInt32 = 0
+    var vers = vers_t()
+    var pLimitData = LimitData_t()
+    var keyInfo = keyInfo_t()
+    var padding: UInt16 = 0
+    var result: UInt8 = 0
+    var status: UInt8 = 0
+    var data8: UInt8 = 0
+    var data32: UInt32 = 0
+    var bytes: SMCBytes_t = (UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                             UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                             UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                             UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                             UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                             UInt8(0), UInt8(0))
+}
+
+internal struct SMCVal_t {
+    var key: String
+    var dataSize: UInt32 = 0
+    var dataType: String = ""
+    var bytes: [UInt8] = Array(repeating: 0, count: 32)
+    
+    init(_ key: String) {
+        self.key = key
+    }
+}
+
+extension FourCharCode {
+    init(fromString str: String) {
+        precondition(str.count == 4)
+        
+        self = str.utf8.reduce(0) { sum, character in
+            return sum << 8 | UInt32(character)
+        }
+    }
+    
+    func toString() -> String {
+        return String(describing: UnicodeScalar(self >> 24 & 0xff)!) +
+               String(describing: UnicodeScalar(self >> 16 & 0xff)!) +
+               String(describing: UnicodeScalar(self >> 8  & 0xff)!) +
+               String(describing: UnicodeScalar(self       & 0xff)!)
+    }
+}
+
+extension Float {
+    init?(_ bytes: [UInt8]) {
+        self = bytes.withUnsafeBytes {
+            return $0.load(fromByteOffset: 0, as: Self.self)
+        }
+    }
+    
+    var bytes: [UInt8] {
+        withUnsafeBytes(of: self, Array.init)
+    }
+}
+
+    
