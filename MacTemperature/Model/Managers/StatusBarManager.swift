9a5ab1b02a619a8e60835c2984de922e1b26e6c1
ighiba <ighiba18@gmail.com>
2023-05-31 01:58:06 +0300

    Added status bar menu item Enable icon switch

--- MacTemperature/Model/Managers/StatusBarManager.swift
+++ MacTemperature/Model/Managers/StatusBarManager.swift
@@ -8,20 +8,44 @@
 import Foundation
 import AppKit
 
+@objc protocol StatusBarDelegate: AnyObject {
+    func enableIconSwitched(sender: NSSwitch)
+}
+
 class StatusBarManager {
     static let shared = StatusBarManager()
     
     private let statusItem: NSStatusItem
+    private var isIconEnabled = true
+    private var currentLevel: TemperatureLevel = .low
+    
+    var menu = NSMenu()
     
     private init() {
         self.statusItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        self.statusItem.button?.target = self
+        self.statusItem.button?.action = #selector(statusBarButtonClicked)
+        
+        let menuItem1 = NSMenuItem(title: "Enable icon", action: #selector(statusBarButtonClicked), keyEquivalent: "")
+        let menuItem2 = NSMenuItem(title: "CPU Temp", action: #selector(statusBarButtonClicked), keyEquivalent: "")
+        menuItem1.view = EnableIconMenuItem(self)
+        //menuItem2.view = CpuTempMenuView()
+        
+        
+        menu.addItem(menuItem1)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(menuItem2)
+        //menu.addItem(menuItem2)
+        
+        
+        statusItem.menu = menu
     }
-    
+
     func updateTemperature(_ floatValue: Float) {
         let stringValue = String(format: "%.0f", floatValue)
         let newTitle = "\(stringValue)°C"
         
-        let currentLevel = TemperatureLevel.getLevel(floatValue)
+        self.currentLevel = TemperatureLevel.getLevel(floatValue)
 
         let attributedTitle = NSMutableAttributedString(string: newTitle)
         let attributesValue: [NSAttributedString.Key: Any] = [.foregroundColor: currentLevel.getColor()]
@@ -33,7 +57,7 @@ class StatusBarManager {
         
         if let button = self.statusItem.button {
             button.attributedTitle = attributedTitle
-            button.image = currentLevel.getImage()
+            button.image = isIconEnabled ? currentLevel.getImage() : nil
         }
     }
     
@@ -43,4 +67,19 @@ class StatusBarManager {
             button.contentTintColor = NSColor.systemTeal
         }
     }
+}
+
+extension StatusBarManager: StatusBarDelegate {
+    
+    @objc func statusBarButtonClicked(_ sender: NSStatusBarButton) {
+        self.statusItem.menu?.popUp(positioning: nil, at: NSPoint(), in: statusItem.button)
+    }
+    
+    @objc func enableIconSwitched(sender: NSSwitch) {
+        isIconEnabled = sender.state == .on
+        if let button = statusItem.button {
+            button.image = isIconEnabled ? currentLevel.getImage() : nil
+        }
+    }
+    
 }
