a4a29d38aea53267d6a341916fec132e77d41709
ighiba <ighiba18@gmail.com>
2023-06-03 14:18:08 +0300

    Added NotificationNames

---
+++ MacTemperature/Model/NotificationNames.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNames.swift
+//  MacTemperature
+//
+//  Created by Ivan Ghiba on 03.06.2023.
+//
+
+import Foundation
+
+class NotificationNames {
+    static let temperatureUpdateNotifaction = Notification.Name("ru.ighiba.TemperatureUpdateNotifaction")
+    static let isEnableStatusBarIconNotification = Notification.Name("ru.ighiba.isEnableStatusBarIconNotification")
+}
