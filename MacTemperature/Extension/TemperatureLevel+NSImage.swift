dfa152ae93f995439ac5fa2d5a4751a702355a79
ighiba <ighiba18@gmail.com>
2023-09-30 20:02:13 +0300

    Code cleaning

--- MacTemperature/Extension/TemperatureLevel+NSImage.swift
+++ MacTemperature/Extension/TemperatureLevel+NSImage.swift
@@ -10,9 +10,12 @@ import Cocoa
 extension TemperatureLevel {
     func getImage() -> NSImage {
         switch self {
-        case .low:    return NSImage(systemSymbolName: "thermometer.low", accessibilityDescription: nil)!
-        case .medium: return NSImage(systemSymbolName: "thermometer.medium", accessibilityDescription: nil)!
-        case .high:   return NSImage(systemSymbolName: "thermometer.high", accessibilityDescription: nil)!
+        case .low:
+            return NSImage(systemSymbolName: "thermometer.low", accessibilityDescription: nil)!
+        case .medium:
+            return NSImage(systemSymbolName: "thermometer.medium", accessibilityDescription: nil)!
+        case .high:
+            return NSImage(systemSymbolName: "thermometer.high", accessibilityDescription: nil)!
         }
     }
 }
