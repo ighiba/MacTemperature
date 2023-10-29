dee75e1509c253a300efb8c5bc2905ca76603f8b
ighiba <ighiba18@gmail.com>
2023-10-29 22:46:31 +0300

    Refactored StatusBarSettingsViewController

--- MacTemperature/Screens/Settings/View/Items/StatusBarSettingsViewController.swift
+++ MacTemperature/Screens/Settings/View/Items/StatusBarSettingsViewController.swift
@@ -7,6 +7,8 @@
 
 import Cocoa
 
+private let popUpButtonSize: NSSize = NSSize(width: 60, height: 30)
+
 class StatusBarSettingsViewController: SettingsItemViewController, SettingsItemView {
         
     var settings: StatusBarSettingsData  { delegate.statusBarSettings }
@@ -19,40 +21,45 @@ class StatusBarSettingsViewController: SettingsItemViewController, SettingsItemV
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        settingsStack.addArrangedSubview(showIcon)
-        settingsStack.addArrangedSubview(avgTemperature)
+        setupViews()
+    }
+    
+    private func setupViews() {
+        settingsStack.addArrangedSubview(showIconContainer)
+        settingsStack.addArrangedSubview(averageTemeperatureSensorContainer)
     }
     
     // MARK: - Views
     
-    lazy var showIcon = SettingsRowContainer(
+    lazy var showIconContainer = SettingsRowContainer(
         title: "Icon",
-        views: [getShowIconCheckboxButton()],
+        views: [configureShowIconCheckbox()],
         width: settingsWidth
     )
     
-    lazy var avgTemperature = SettingsRowContainer(
+    lazy var averageTemeperatureSensorContainer = SettingsRowContainer(
         title: "Average temperature for",
-        views: [getPopUpButton()],
+        views: [configureTemperatureSensorsPopUpButton()],
         width: settingsWidth
     )
     
-    private func getShowIconCheckboxButton() -> NSButton {
+    private func configureShowIconCheckbox() -> NSButton {
         let button = NSButton(
             checkboxWithTitle: "Show thermometer icon",
             target: self,
-            action: #selector(showIconCheckboxChanged)
+            action: #selector(showIconCheckboxStateDidChange)
         )
         button.state = settings.statusBarShowIcon ? .on : .off
+        
         return button
     }
     
-    func getPopUpButton() -> NSPopUpButton {
-        let popUpButton = NSPopUpButton(frame: NSRect(x: 0, y: 0, width: 60, height: 30))
-        
-        popUpButton.addItems(withTitles: TemperatureSensorType.allCases.map({ $0.stringValue }))
+    private func configureTemperatureSensorsPopUpButton() -> NSPopUpButton {
+        let popUpButton = NSPopUpButton(frame: NSRect(origin: .zero, size: popUpButtonSize))
+        let titles = TemperatureSensorType.allCases.map { $0.stringValue }
+        popUpButton.addItems(withTitles: titles)
         popUpButton.selectItem(at: settings.statusBarAverageTemperatureFor.rawValue)
-        popUpButton.action = #selector(popUpButtonDidChanged)
+        popUpButton.action = #selector(selectedAverageTemperatureSensorDidChange)
         popUpButton.target = self
         
         return popUpButton
@@ -62,12 +69,12 @@ class StatusBarSettingsViewController: SettingsItemViewController, SettingsItemV
 // MARK: - Actions
 
 extension StatusBarSettingsViewController {
-    @objc func showIconCheckboxChanged(_ sender: NSButton) {
+    @objc func showIconCheckboxStateDidChange(_ sender: NSButton) {
         settings.statusBarShowIcon = sender.state == .on ? true : false
         delegate.setStatusBarSettings(settings)
     }
 
-    @objc func popUpButtonDidChanged(_ sender: NSPopUpButton) {
+    @objc func selectedAverageTemperatureSensorDidChange(_ sender: NSPopUpButton) {
         settings.statusBarAverageTemperatureFor = TemperatureSensorType(rawValue: sender.indexOfSelectedItem) ?? .cpu
         delegate.setStatusBarSettings(settings)
     }
